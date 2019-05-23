6d890fb1198daac5cad41a657447632eb234dc72
DESKTOP-8TAC9F7\My Computer <dyp.dimasyp@gmail.com>
2019-05-23 21:01:35 +0700

    Rekap Keuangan

--- application/controllers/Rekap_keuangan.php
+++ application/controllers/Rekap_keuangan.php
@@ -11,4 +11,56 @@ Class Rekap_keuangan extends CI_Controller{
         $data['keuangan'] = $this->M_keuangan->getKeuangan();
         $this->load->view('backend/master-template',$data);
     }
+    function add(){
+        $config = array(
+            array(
+                'field' => 'tanggal',
+                'label' => 'Tanggal',
+                'rules' => 'required|max_length[19]|min_length[19]'
+            ),
+            array(
+                'field' => 'tipe',
+                'label' => 'Tipe',
+                'rules' => 'required'
+            ),
+            array(
+                'field' => 'detail_info',
+                'label' => 'Detail Info',
+                'rules' => 'required'
+            ),
+            array(
+                'field' => 'jumlah',
+                'label' => 'Jumlah',
+                'rules' => 'required|integer'
+            ),
+        );
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run() == TRUE){
+            $tipe = $this->input->post('tipe');
+            //Format Tanggal Dan Waktu
+            $postTanggal = $this->input->post('tanggal');
+            $tanggal = substr($postTanggal,0,10);
+            $tanggal =  date("Y-m-d",strtotime($tanggal));
+            $waktu = substr($postTanggal,11);
+            $postTanggal = $tanggal." ".$waktu;
+            //Input Ke Database
+            $data = array(
+                'tanggal' => $postTanggal,
+                'detail_info' => $this->input->post('detail_info'),
+                'jumlah' => $this->input->post('jumlah')
+            );
+            if($tipe=="debit"){ //Pemasukan
+                $this->M_keuangan->inputPemasukan($data);
+            }else if($tipe=="kredit"){ //Pengeluaran
+                $this->M_keuangan->inputPengeluaran($data);
+            }
+            $this->session->set_flashdata("input_success","<div class='alert alert-success'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>Data Berhasil Ditambahkan!!</div>");
+        }else{
+            $gagal = validation_errors();
+            $this->session->set_flashdata("input_failed","<div class='alert alert-danger'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>Data Gagal Ditambahkan!!<br>".$gagal."</div>");
+        }
+        redirect('rekap_keuangan');
+    }
 }
--- application/views/backend/keuangan/rekap_keuangan.php
+++ application/views/backend/keuangan/rekap_keuangan.php
@@ -2,6 +2,12 @@
     $(document).ready(function(){
         $("#primary-table").DataTable();
     });
+    $(function () {
+        $('#dtpickerdemo').datetimepicker({
+            format: 'DD-MM-YYYY hh:mm:ss',
+            locale: 'en'
+        });
+    });
 </script>
 <div class="container-fluid">
     <div class="row">
@@ -12,10 +18,23 @@
                         <div class="pull-left">
                             <h1>Rekap Keuangan</h1>
                         </div>
+                        <div class="pull-right">
+                            <button type="button" class="btn btn-sm btn-primary" href="#" data-toggle="modal" data-target="#modaladd"><i class="fa fa-filter"></i> Filter</button>
+                            <button type="button" class="btn btn-sm btn-primary" href="#" data-toggle="modal" data-target="#modaladd"><i class="fa fa-plus"></i> Tambah</button>
+                        </div>
                         
                     </div>
                 </div>
                 <div class="sparkline8-graph">
+                    <?php
+                        echo $this->session->flashdata("input_success");
+                        echo $this->session->flashdata("input_failed");
+                        echo $this->session->flashdata("update_success");
+                        echo $this->session->flashdata("update_failed");
+                        echo $this->session->flashdata("delete_success");
+                        echo $this->session->flashdata("delete_failed");
+                    
+                    ?>
                     <div class="static-table-list">
                         <table id="primary-table" class="table">
                             <thead>
@@ -45,6 +64,58 @@
                         </table>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div id="modaladd" class="modal modal-edu-general default-popup-PrimaryModal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="<?php echo base_url()."index.php/rekap_keuangan/add" ?>" method="post">
+                    <div class="modal-header header-color-modal bg-color-1">
+                        <h4 class="modal-title">Tambah Data Pemasukan/Pengeluaran Lain</h4>
+                        <div class="modal-close-area modal-close-df">
+                            <a class="close" data-dismiss="modal" href="#"><i class="fa fa-close"></i></a>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group data-custon-pick">
+                                    <label style="font-weight:bold;">Pilih Tanggal</label>
+                                    <div class='input-group date' id='dtpickerdemo'>
+                                        <input type="text" name="tanggal" class="form-control" value="<?php echo date("dd/mm/YYYY H:i:s") ?>" required />
+                                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="pull-left">Tipe</label>
+                                    <select class="form-control" name="tipe" required>
+                                        <option value="debit">Debit</option>
+                                        <option value="kredit">Kredit</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="pull-left">Detail Penggunaan</label>
+                                    <textarea class="form-control" name="detail_info" style="width: 100%;min-width:100%;max-width:100%" required></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="pull-left">Jumlah</label>
+                                    <input type="number" class="form-control" name="jumlah" required>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input class="btn btn-sm btn-default" type="reset" value="Reset">
+                        <input class="btn btn-sm btn-primary" type="submit" value="Simpan">
+                    </div>
+                </form> 
             </div>
         </div>
     </div>
